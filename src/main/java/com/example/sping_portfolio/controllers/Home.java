90c7a555a0aba20935ce8c6c83817ad83d033adb
Rini K <89210546+rkwreck@users.noreply.github.com>
2021-09-04 21:03:32 -0700

    adjusted random number generator (pop-up)

---
+++ src/main/java/com/example/sping_portfolio/controllers/Home.java
@@ -0,0 +1,17 @@
+//package com.example.sping_portfolio.controllers;
+
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+
+//@Controller  /*
+ //HTTP requests are handled as a controller, using the @Controller annotation
+//public class Home {
+ //<h2> </h2>
+ // <p>
+//  <a href="default.about">
+// <img src="about.giraffe" alt="about us" style="width:150px;"
+// </a>
+//  </html>
+//}
+//*/
+
