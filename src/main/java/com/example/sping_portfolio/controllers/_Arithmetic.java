e5d98d4f8edec36ff17566c0e2fcd5eb0ec5c131
chenxin-chex <chenxinni0@gmail.com>
2021-09-28 09:32:16 -0700

    main abstract parent class Arithmetic

---
+++ src/main/java/com/example/sping_portfolio/controllers/_Arithmetic.java
@@ -0,0 +1,69 @@
+package com.example.sping_portfolio.controllers;
+
+
+import lombok.Getter;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Getter
+public abstract class _Arithmetic {
+    int size;
+    String name;
+    int hashID;
+    Duration timeElapsed;
+    ArrayList<Long> list;
+    HashMap<Integer, Object> hash;
+
+    public _Arithmetic(int nth) {
+        this.size = nth;
+        this.list = new ArrayList<>();
+        this.hashID = 0;
+        this.hash = new HashMap<>();
+        // initialize arithmetic and time algorithm
+        Instant start = Instant.now();
+        this.init();
+        Instant end = Instant.now();
+        this.timeElapsed = Duration.between(start, end);
+    }
+
+    protected abstract void init();
+
+    public void setData(long num) {
+        list.add(num);
+        hash.put(this.hashID++, list.clone());
+    }
+
+    public int getTimeElapsed() {
+        return timeElapsed.getNano();
+    }
+
+    public long getNth() {
+        return list.get(size - 1);
+    }
+
+    public Object getNthSeq(int i) {
+        return hash.get(i);
+    }
+
+    public ArrayList<Long> getList() {
+        return list;
+    }
+
+    public HashMap<Integer, Object> getHash() {
+        return hash;
+    }
+
+    public void print() {
+        ConsoleMethods.println("Init method = " + this.name);
+        ConsoleMethods.println("Init time = " + this.getTimeElapsed());
+        ConsoleMethods.println("Fibonacci Number " + this.size + " = " + this.getNth());
+        ConsoleMethods.println("Fibonacci List = " + this.getList());
+        ConsoleMethods.println("Fibonacci Hashmap = " + this.getHash());
+        for (int i=0 ; i<this.size; i++ ) {
+            ConsoleMethods.println("Fibonacci Sequence " + (i+1) + " = " + this.getNthSeq(i));
+        }
+    }
+
+}
