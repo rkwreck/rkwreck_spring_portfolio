e41a29f23f15ac7a87663dd5123a206ddb0addbc
rachelklee <77082379+rachelklee@users.noreply.github.com>
2021-10-11 08:53:37 -0700

    modeling

--- src/main/java/com/example/sping_portfolio/controllers/modeling/modeling.java
+++ src/main/java/com/example/sping_portfolio/controllers/modeling/modeling.java
@@ -15,6 +15,10 @@ import javax.imageio.ImageIO;
 public class modeling {
 
     public String file, url;
+    public int scale_factor;
+    public int height, scaled_height;
+    public int width, scaled_width;
+    public int[][][] rgb_matrix;
 
     public modeling(String file, String url) {
 
@@ -43,14 +47,19 @@ public class modeling {
 
         ArrayList<iprop> list = new ArrayList<iprop>();
         byte[] pixels_int = image_to_pixels(img);
-        String base64 = pixels_to_base64(img.getWidth(), img.getHeight(), pixels_int);
-        iprop original = new iprop("original info", base64);
+        String orbase64 = pixels_to_base64(img.getWidth(), img.getHeight(), pixels_int);
+        iprop original = new iprop("original info", orbase64);
+        byte[] pixels_int2 = grayscale(pixels_int);
+        String gsbase64 = pixels_to_base64(img.getWidth(), img.getHeight(), pixels_int2);
+        iprop gray = new iprop("greyscale info", gsbase64);
+
 
 
 
         public void add() {
 
             list.add(original);
+            list.add(gray);
         }
         ///
 
@@ -66,31 +75,37 @@ public class modeling {
             this.base64 = base;
         }
 
+        ArrayList<iprgb> rgb = new ArrayList<iprgb>();
 
-
-
+        public void add() {
+            rgb.add(iprgb.int);
+        }
     }
 
     public class iprgb {
 
+        public int[][][] rgb() {
+            int intArray[][][] = new int[20][][];
+            return intArray;
+
+        }
+
+        public String hexa() {
+            return "hello";
+
+        }
+
+        public int[][][] binary() {
+            int intArray[][][] = new int[20][][];
+            return intArray;
+
+        }
+
+
+
     }
 
-    public int[][][] rgb() {
-        int intArray[][][] = new int[20][][];
-        return intArray;
 
-    }
-
-    public String hexa() {
-        return "hello";
-
-    }
-
-    public int[][][] binary() {
-        int intArray[][][] = new int[20][][];
-        return intArray;
-
-    }
      /////
 
     public String pixels_to_base64(int width, int height, byte[] pixels) throws IOException {
@@ -120,6 +135,60 @@ public class modeling {
     public byte[] image_to_pixels(BufferedImage img) throws IOException {
         byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
         return pixels;
+    }
+
+    public byte[] grayscale(byte[] pixels){
+        byte[] pixels_int = new byte[pixels.length];
+        for(int i=0;i<pixels.length;i+=4) {
+            float val = 0;
+            for(int y=1;y<4;y++) {
+                val += (pixels[i+y] & 0xFF)/3.0; // The & 0xFF is a "bitwise and" https://www.geeksforgeeks.org/bitwise-operators-in-java/. Just divigin by 3 to get the average
+            }
+            pixels_int[i+0] = pixels[i]; // keep alpha the same
+            pixels_int[i+1] = (byte)val;
+            pixels_int[i+2] = (byte)val;
+            pixels_int[i+3] = (byte)val;
+        }
+        return pixels_int;
+    }
+
+
+
+    public int getScaled_height(int row) {
+        return row * this.scale_factor;
+    }
+
+    public int getScaled_width(int col) {
+        return col * this.scale_factor;
+    }
+    public String getRGB(int row, int col) {
+        int h = row * this.scale_factor;
+        int w = col * this.scale_factor;
+        return "(" + rgb_matrix[h][w][0] +
+                "," + rgb_matrix[h][w][1] +
+                "," + rgb_matrix[h][w][2] +
+                ")" ;
+    }
+
+    public String getHexCode(int row, int col) {
+        int h = row * this.scale_factor;
+        int w = col * this.scale_factor;
+        // String.format guarantees 0 padding vs Integer.toHexString
+        return "#" +
+                String.format("%02X",rgb_matrix[h][w][0]) +
+                String.format("%02X",rgb_matrix[h][w][1]) +
+                String.format("%02X",rgb_matrix[h][w][2]) ;
+    }
+
+    public String getBinary(int row, int col) {
+        int h = row * this.scale_factor;
+        int w = col * this.scale_factor;
+        // Java does not have binary as string formatter
+        return  String.format("%8s", Integer.toBinaryString(rgb_matrix[h][w][0])).replace(' ', '0') +
+                " " +
+                String.format("%8s", Integer.toBinaryString(rgb_matrix[h][w][1])).replace(' ', '0') +
+                " " +
+                String.format("%8s", Integer.toBinaryString(rgb_matrix[h][w][2])).replace(' ', '0') ;
     }
 
 }
