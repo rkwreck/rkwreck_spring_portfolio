0d45c175107e0cb85c6dff7f3910aa810941cee2
chenxin-chex <chenxinni0@gmail.com>
2021-10-10 19:32:37 -0700

    start model

--- src/main/java/com/example/sping_portfolio/controllers/modeling/modeling.java
+++ src/main/java/com/example/sping_portfolio/controllers/modeling/modeling.java
@@ -1,6 +1,11 @@
 package com.example.sping_portfolio.controllers.modeling;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import lombok.Getter;
+import org.apache.tomcat.util.codec.binary.Base64;
+
 import java.net.URL;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
@@ -15,24 +20,37 @@ public class modeling {
 
     }
 
-    public class lii {
+    ArrayList<ii> lii = new ArrayList<>();
 
-        ArrayList<ii> lii = new ArrayList<ii>();
-
+    public void add() throws IOException {
+        ii file0 = new ii("/images/Mona_Lisa.png");
+        lii.add(file0);
+        ii file1 = new ii("/images/bulb_on.gif");
+        lii.add(file1);
+        ii file2 = new ii("/bulb_off.png");
+        lii.add(file2);
     }
+
+
 
     public class ii {
         public String file;
+        BufferedImage img = ImageIO.read(new URL(url));
 
-        public ii(String file) {
+        public ii(String file) throws IOException {
             this.file = file;
         }
 
         ArrayList<iprop> list = new ArrayList<iprop>();
-        iprop test = new iprop("hello", "hi");
+        byte[] pixels_int = image_to_pixels(img);
+        String base64 = pixels_to_base64(img.getWidth(), img.getHeight(), pixels_int);
+        iprop original = new iprop("original info", base64);
+
+
 
         public void add() {
-            list.add(test);
+
+            list.add(original);
         }
 
 
@@ -46,6 +64,8 @@ public class modeling {
             this.description = descrip;
             this.base64 = base;
         }
+
+
 
 
     }
@@ -71,6 +91,35 @@ public class modeling {
 
     }
      /////
+
+    public String pixels_to_base64(int width, int height, byte[] pixels) throws IOException {
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        for(int y=0; y<height;y++) {
+            for(int x=0; x<width; x++) {
+                int a,r,g,b;
+                int s = (y*width + x)*4;
+                a=pixels[s+0];
+                r=pixels[s+1];
+                g=pixels[s+2];
+                b=pixels[s+3];
+                img.setRGB(x,y, argb(a,r,g,b));
+            }
+        }
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ImageIO.write(img, "gif", bos);
+        byte[] data = bos.toByteArray();
+        return Base64.encodeBase64String(data);
+    }
+
+    public int argb(int a, int r, int g, int b) {
+        return ((a&0x0ff)<<24)|((r&0x0ff)<<16)|((g&0x0ff)<<8)|(b&0x0ff);
+    }
+
+    public byte[] image_to_pixels(BufferedImage img) throws IOException {
+        byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+        return pixels;
+    }
 
 }
 
