aa27fcd7493c2fd3c012be3ab56f00142f558b7c
chenxin-chex <chenxinni0@gmail.com>
2021-10-10 18:51:39 -0700

    start image controller

--- src/main/java/com/example/sping_portfolio/controllers/modeling/modeling.java
+++ src/main/java/com/example/sping_portfolio/controllers/modeling/modeling.java
@@ -1,33 +1,75 @@
 package com.example.sping_portfolio.controllers.modeling;
+import java.util.ArrayList;
+import lombok.Getter;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
+@Getter
 public class modeling {
+
+    public String file, url;
+
+    public modeling(String file, String url) {
+
+    }
 
     public class lii {
 
-        public class ii {
+        ArrayList<ii> lii = new ArrayList<ii>();
 
-            public class iprop {
+    }
 
-                public class iprgb {
+    public class ii {
+        public String file;
 
-                    public class rgb {
-
-                    }
-
-                    public class hexa {
-
-                    }
-
-                    public class binary {
-
-                    }
-
-                }
-
-            }
-
+        public ii(String file) {
+            this.file = file;
         }
+
+        ArrayList<iprop> list = new ArrayList<iprop>();
+        iprop test = new iprop("hello", "hi");
+
+        public void add() {
+            list.add(test);
+        }
+
+
+    }
+
+    public class iprop {
+        public String description;
+        public String base64;
+
+        public iprop(String descrip, String base) {
+            this.description = descrip;
+            this.base64 = base;
+        }
+
+
+    }
+
+    public class iprgb {
+
+    }
+
+    public int[][][] rgb() {
+
+
+
+    }
+
+    public String hexa() {
+
+    }
+
+    public int[][][] binary() {
 
     }
 
 }
+
+
+
+
