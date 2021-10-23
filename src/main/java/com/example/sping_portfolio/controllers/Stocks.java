037ced152534f7844316da61868fa25efa1929e9
Rini K <89210546+rkwreck@users.noreply.github.com>
2021-10-22 17:36:03 -0700

    refactored html files (removed categorizations)

--- src/main/java/com/example/sping_portfolio/controllers/Stocks.java
+++ src/main/java/com/example/sping_portfolio/controllers/Stocks.java
@@ -3,15 +3,12 @@ package com.example.sping_portfolio.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Stocks {
     @GetMapping("/Stocks")
-
-
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String Stocks() {
-        return "Stocks"; // returns HTML VIEW (greeting)
+        return "/Stocks"; // returns HTML VIEW (greeting)
     }
 }
 
