e72efe9d936ae48a9982c052d22499c7ce1e4071
rachelklee <77082379+rachelklee@users.noreply.github.com>
2021-10-31 14:30:44 -0700

    login page

---
+++ src/main/java/com/example/sping_portfolio/controllers/loginPage/Main.java
@@ -0,0 +1,9 @@
+package com.example.sping_portfolio.controllers.loginPage;
+
+public class Main {
+    public static void main(String[] args) {
+        //create the object of Login Page to see its working
+        new loginPage();
+    }
+
+}
--- src/main/java/com/example/sping_portfolio/controllers/loginPage/loginPage.java
+++ src/main/java/com/example/sping_portfolio/controllers/loginPage/loginPage.java
@@ -1,10 +1,28 @@
 package com.example.sping_portfolio.controllers.loginPage;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class loginPage {
-    private JTextField textField1;
+public class loginPage extends JFrame{
+    private JTextField userText;
     private JPanel panel1;
-    private JPasswordField passwordField1;
-    private JButton button1;
+    private JPasswordField password;
+    private JButton loginButton;
+    private JFrame frame;
+
+    public loginPage(){
+
+        frame=new JFrame("Login Frame");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(250, 200));
+        frame.setResizable(false);
+
+        //now add the panel
+        frame.add(panel1);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
 }
