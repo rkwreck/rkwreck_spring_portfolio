c523f7945d6e96bb036649bea210bbf7043dba10
rachelklee <77082379+rachelklee@users.noreply.github.com>
2021-11-03 10:50:24 -0700

    register 1

--- src/main/java/com/example/sping_portfolio/controllers/Register.java
+++ src/main/java/com/example/sping_portfolio/controllers/Register.java
@@ -1,25 +1,94 @@
+
 package com.example.sping_portfolio.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.example.sping_portfolio.controllers.lucasModel.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import java.util.List;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Register {
-
     @GetMapping("/register")
-    public String register(@RequestParam(name="account", required=true) String account, Model model) {
+    public String register(@RequestParam(name = "firstname", required = false) String firstname, @RequestParam(name = "password", required = false) String password, Model model) {
+        System.out.println("firstname from form " + firstname);
+        System.out.println("password from form " + password);
 
-        // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-        return "Register"; // returns HTML VIEW (greeting)
+
+        return "register"; // returns HTML VIEW (greeting)
     }
+
+    /**
+     * Connect to database
+     */
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:src/main/resources/static/database/mydatabase.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Register: Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
+    public void inputFirstnamePassword(String firstname, String password) {
+        String sql = "INSERT INTO rc_table(?,?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, firstname);
+            pstmt.setString(2, password);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+/*
+    // Check username and password
+    public static boolean inputFirstnamePassword(String firstname, String password) {
+        String sql = "INSERT INTO rc_table VALUES(?,?); ";
+
+        try (Connection conn = connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // set the corresponding param
+            pstmt.setString(1, firstname);
+            pstmt.setString(2, password);
+
+            // update
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                System.out.println(firstname + " with password " + password + " registration successful");
+                return true;
+            } else {
+                System.out.println(firstname + " with password " + password + " registration failed");
+                return false;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+ */
+
 }
 
 
